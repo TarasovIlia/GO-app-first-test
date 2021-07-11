0c96eab8c7239ba354e613f3f1ba0a0cfae06dc3
TarasovIlia <tarasov002@gmail.com>
2021-07-11 17:23:33 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/hello
+
+go 1.16
+
+require github.com/maxence-charriere/go-app/v8 v8.0.4 // indirect
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/maxence-charriere/go-app/v8/pkg/app"
+)
+
+type hello struct {
+	app.Compo
+}
+
+func (h *hello) Render() app.UI {
+	return app.H1().Text("Hello World!")
+}
+
+func main() {
+
+	app.Route("/", &hello{})
+
+	app.RunWhenOnBrowser()
+
+	http.Handle("/", &app.Handler{
+		Name:        "Hello",
+		Description: "An Hello World! example",
+	})
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
